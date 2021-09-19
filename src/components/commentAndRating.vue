5800867c78db045c6e9f0a1ced197a6bfc89fae5
Chase Johnston <46426781+blueberrycola@users.noreply.github.com>
2021-09-19 19:55:14 -0400

    beginning work on comment+rating

---
+++ src/components/commentAndRating.vue
@@ -0,0 +1,24 @@
+<template>
+    <div classname="commentRating">
+        <header>Leave a Comment and Rating!</header>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "commentRating",
+
+    }
+</script>
+
+<style scoped>
+    header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 20px;
+    }
+</style>
+
