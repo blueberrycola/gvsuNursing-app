b4dced6e8e6d341b0daa53c1b654ca23c50c0056
baileylucasd <35927184+baileylucasd@users.noreply.github.com>
2021-09-26 14:15:48 -0400

    Add files via upload

---
+++ app/src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click=onClick() class="btn">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String
+        
+    },
+    methods: {
+        onClick() {
+            console.log('clicc');
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/src/components/questions.vue
@@ -0,0 +1,25 @@
+<template>
+    <h2>
+    <div :key="question.id" v-for="question in questions">
+        <Question :question="question"></Question>
+        <Radio></Radio>
+    </div>
+    </h2>
+
+</template>
+
+<script>
+
+    import Question from './question.vue'
+    import Radio from './Radio.vue'
+    export default {
+        name: 'Questions',
+        props: {
+            questions: Array
+        },
+        components: {
+            Question,
+            Radio
+        }
+    }
+</script>
