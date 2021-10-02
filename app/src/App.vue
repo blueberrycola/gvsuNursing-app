aa648ca6c9d56f5c2bc521e566995d9d3280bbeb
Chase Johnston <46426781+blueberrycola@users.noreply.github.com>
2021-10-02 16:04:34 -0400

    updated

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,13 +3,12 @@
     
     <Questions :questions="Questions"/>
     <Button text="Submit"></Button>
-    <commentRating/>
 
   </div>
 </template>
 
 <script>
-import commentRating from './components/commentAndRating.vue'
+
 import Questions from './components/questions.vue'
 import Button from './components/Button.vue'
 
@@ -17,7 +16,6 @@ import Button from './components/Button.vue'
 export default {
   name: 'App',
   components: {
-    commentRating,
     Questions,
     Button,
   },
@@ -31,85 +29,12 @@ export default {
       //FUTURE PLANS
       {
         id: 1,
-        text: 'Do you plan to pursue a graduate degree?',
-      },
-      {
-        id: 2,
-        text: 'Do you have any organizational requirements for future studies?'
-      },
-      //FORMAT
-      {
-        id: 3,
-        text: 'Are you undecided on your future plans at this time?'
-      },
-      {
-        id: 4,
-        text: 'Do you have a stable internet connection? Do you like remote learning?'
-      },
-      {
-        id: 5,
-        text: 'What type of learner are you?' //add input audio/visual, both
-      },
-      {
-        id: 6,
-        text: 'Do you like group projects?'
-      },
-      {
-        id: 7,
-        text: 'Do you like enjoy the benefits of online learning but still want some face to face interaction?'
-      },
-      //ETC
-      {
-        id: 8,
-        text: 'What is your age?'
-      },
-      
-      
-      //FINANCE
-      {
-        id: 12,
-        text: 'Do you possess any scholarships?'
-      },
-      {
-        id: 13,
-        text: 'Do you finance your own education'
-      },
-      {
-        id: 14,
-        text: 'Do you have any financial loans for college'
-      },
-      {
-        id: 15,
-        text: "Repayment terms for loan"
-      },
-      {
-        id: 16,
-        text: "Does your employer pay for your college?"
-      },
-      //Transfer Credits
-      {
-        id: 17,
-        text: 'Are you a transfer student'
-      },
-      {
-        id: 18,
-        text: 'Do you have an affiliation or concurrent enrollment agreements'
-      },
-      {
-        id: 19,
-        text: 'Are you okay with all the gen-ed courses you will take?'
-      },
-      //TIME
-      {
-        id: 11,
-        text: 'Do prefer the faster paced semesters?'
-      },
-      {
-        id: 12,
-        text: 'Do you possess a fixed or flexible work schedule?'
-      },
+        text: 'All questions have moved to treeDisplay.vue, this is now used for homepage',
+      }
       //ORGANIZATIONAL RESOURCES
-      
+      //todo: Affiliation input, one question per page, progress bar, affiliation, 5 star rating, last updated
+      //gvsu branding include but for all, robert talbot gvsu math example https://rtalbert.org/about/, gvsu stock imgs,
+      //spam filter, adminstration tool, digital hosting cloud, 
 
 
 
