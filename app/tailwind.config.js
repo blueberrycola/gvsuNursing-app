f387831974cae60936fe741ce8a0225f81384e4d
baileylucasd <35927184+baileylucasd@users.noreply.github.com>
2021-10-17 12:40:00 -0400

    tailwind config files

---
+++ app/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    Header: {
+      center: true,
+      
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
