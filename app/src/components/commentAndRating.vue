3ff77ea14c28f69146e3a04b202e6e43b8a8f8d9
Chase Johnston <46426781+blueberrycola@users.noreply.github.com>
2021-10-16 20:20:07 -0400

    added css to comment+rating

--- app/src/components/commentAndRating.vue
+++ app/src/components/commentAndRating.vue
@@ -1,20 +1,31 @@
 <template>
-    <div classname="commentRating">
-        <header>Leave a Comment and Rating!</header>
-        <form>
-            <label for="fullname">Full name: </label>
-            <input type="text" id="fname" name="fname"><br><br>
-            <label for="email">Email: </label>
-            <input type="text" id="email" name="email"><br><br>
-            <label for="rating">Rating (Scored between 0-10 pts): </label>
-            <input type="range" id="rating" name="rating" min="1" max="10"><br><br>
-            <label for="nursingreview">Comments: </label>
-            <textarea id="nursingreview" name="nursingreview" rows="4" cols="50">
-            </textarea><br><br>
-            <button @click=submitForm()>
-                Submit Review
-            </button>
-        </form>
+    <div id="commentRating-page">
+        <div id="form-container">
+            
+            <div class="form-main">
+                <h1>Leave a Comment and Rating!</h1>
+                <div class="form-header">
+                    
+                
+                </div>
+            <form>
+                    <h4>Full Name:</h4>
+                    <input type="text" id="fname" name="fname">
+                    <h4>Email:</h4>
+                    <input type="text" id="email" name="email"><br><br>
+                    <h4>Rating:</h4>
+                    <input type="range" id="rating" name="rating" min="1" max="10"><br><br>
+                    <h4>Comments:</h4>
+                    <textarea id="nursingreview" name="nursingreview" rows="4" cols="50">
+                    </textarea><br><br>
+                    <button @click=submitForm()>
+                        Submit Review
+                    </button>
+            </form>
+            </div>
+
+        </div>
+
     </div>
 </template>
 
@@ -34,20 +45,58 @@
                 rating: rating,
                 review: review,
             };
+            //Fixme: submit json to S3 Bucket
             console.log(form);
         }
     }
     }
 
-
 </script>
 
-<style scoped>
-    header {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      margin-bottom: 20px;
+<style>
+    .form-header {
+        margin: 10px;
     }
+    .commentRating-page{
+        display: flex;
+        width: 100%;
+        height: 100;
+        
+    }
+    .form-container {
+        display: flex;
+        font-family: 'Poppins', sans-serif;
+        width: 40%;
+        height: 85%;
+        margin-top: 40px;
+        margin-left: 560px;
+        border: 1px solid;
+        box-shadow: 5px 10px black;
+    }
+    .form-main {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 40%;
+        height: 85%;
+        margin-top: 40px;
+        margin-left: 560px;
+        position: absolute;
+        flex-flow: column;
+        border: 1px solid #e7eae0;
+        border-radius: 10px;
+        background-color: skyblue;
+        box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+    }
+    
+    .form-header {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        display: flex;
+        width: 100%;
+        height: 10%;
+    }
+
 </style>
 
