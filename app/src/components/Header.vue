cfeb0e7f48088d4f91135d99a7d8014f5295ad2c
baileylucasd <35927184+baileylucasd@users.noreply.github.com>
2021-10-17 12:42:43 -0400

    Tailwind / Question revamp

---
+++ app/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header class="text-center bg-blue-800 text-white p-4 mb-10 rounded-2xl">
+        <div class="text-3xl md:text-5xl font-bold mb-3">GVSU Nursing Program Application</div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
