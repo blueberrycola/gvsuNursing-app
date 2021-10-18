0adc02396a0a43c52f2bbf0bbb9609783bf2b3e9
baileylucasd <35927184+baileylucasd@users.noreply.github.com>
2021-10-17 21:19:22 -0400

    app.vue and main.js changes

---
+++ app/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class="container" >
+    <treeDisplay />
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+import treeDisplay from './components/treeDisplay.vue';
+
+export default {
+  name: 'App',
+  components: {
+    treeDisplay
+  },
+  data(){
+    return{
+      Questions: []
+    }
+  },
+  created(){
+    
+      //ORGANIZATIONAL RESOURCES
+      //todo: Affiliation input, one question per page, progress bar, affiliation, 5 star rating, last updated
+      //gvsu branding include but for all, robert talbot gvsu math example https://rtalbert.org/about/, gvsu stock imgs,
+      //spam filter, adminstration tool, digital hosting cloud, 
+
+
+
+    
+  }
+}
+
+</script>
+
+<style>
+.app {
+  font-family: 'Poppins', sans-serif;
+  margin: auto;
+  letter-spacing: 2px;
+}
+</style>
