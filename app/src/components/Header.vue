b9c1a1e0f8e48ddf35e7659b79a2e8272b4602d4
baileylucasd <baileylucasd@gmail.com>
2021-11-06 16:10:00 -0400

    Changed header to GV Blue which is accessible in the tailwind config

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="text-center bg-blue-800 text-white p-4 mb-10 rounded-2xl">
+    <header class="text-center bg-gvsuBlue text-white p-4 mb-10 rounded-2xl">
         <div class="text-3xl md:text-5xl font-bold mb-3">AN to BSN Decision Making Tool</div>
     </header>
 </template>
